9cc1125feeb1688636ad0e92100dc8a7edcd206b
Odili1 <ezedigwegerald1@gmail.com>
2024-09-17 10:42:26 +0100

    add PORT to .env

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,35 +1,33 @@
-import { config } from "dotenv";
-config()
+import { config } from 'dotenv';
+config();
 
-
-function getEnv(envKey: string): any{
-    return process.env[envKey] ?? undefined
+function getEnv(envKey: string): any {
+  return process.env[envKey] ?? undefined;
 }
 
-function getEnvString(envKey: string): string{
-    const val = getEnv(envKey)
-    // console.log(`${envKey}: ${val}`);
-    
-    return val || ''
+function getEnvString(envKey: string): string {
+  const val = getEnv(envKey);
+  // console.log(`${envKey}: ${val}`);
+
+  return val || '';
 }
 
-function getEnvNumber(envKey: string, defaultValue?: number){
-    const val = getEnv(envKey)
-    // console.log(`${envKey}: ${val}`);
-    return val !== undefined && !isNaN(Number(val)) ? Number(val) : defaultValue
+function getEnvNumber(envKey: string, defaultValue?: number) {
+  const val = getEnv(envKey);
+  // console.log(`${envKey}: ${val}`);
+  return val !== undefined && !isNaN(Number(val)) ? Number(val) : defaultValue;
 }
 
 export const envConfig = {
-    DB_NAME: getEnvString('DB_NAME'),
-    DB_HOST: getEnvString('DB_HOST'),
-    DB_USERNAME: getEnvString('DB_USER'),
-    DB_TYPE: getEnvString('DB_TYPE'),
-    DB_PASSWORD: getEnvString('DB_PASSWORD'),
-    DB_PORT: getEnvNumber('DB_PORT', 3306),
-    JWT_SECRET: getEnvString('JWT_SECRET'),
-    CLOUDINARY_NAME: getEnvString('CLOUDINARY_NAME'),
-    CLOUDINARY_API_KEY: getEnvString('CLOUDINARY_API_KEY'),
-    CLOUDINARY_API_SECRET: getEnvString('CLOUDINARY_API_SECRET')
-}
-
-
+  DB_NAME: getEnvString('DB_NAME'),
+  DB_HOST: getEnvString('DB_HOST'),
+  DB_USERNAME: getEnvString('DB_USER'),
+  DB_TYPE: getEnvString('DB_TYPE'),
+  DB_PASSWORD: getEnvString('DB_PASSWORD'),
+  DB_PORT: getEnvNumber('DB_PORT', 3306),
+  JWT_SECRET: getEnvString('JWT_SECRET'),
+  CLOUDINARY_NAME: getEnvString('CLOUDINARY_NAME'),
+  CLOUDINARY_API_KEY: getEnvString('CLOUDINARY_API_KEY'),
+  CLOUDINARY_API_SECRET: getEnvString('CLOUDINARY_API_SECRET'),
+  PORT: getEnvNumber('PORT', 3000),
+};
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser'
+import * as cookieParser from 'cookie-parser';
+import { envConfig } from './config/envConfig';
 // import { ValidationPipe } from '@nestjs/common';
 // import { CommandFactory } from 'nest-commander';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // await CommandFactory.run(AppModule, ['warn', 'error'])
-  app.enableCors()
-  app.use(cookieParser())
+  app.enableCors();
+  app.use(cookieParser());
 
-  await app.listen(4850);
+  await app.listen(envConfig.PORT || 3000);
 }
 bootstrap();
